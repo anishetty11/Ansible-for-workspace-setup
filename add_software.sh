a0d2399977e680bb9d85d2f17e4851c949bb45bf
Anish <anishshetty@live.com>
2017-12-09 17:43:16 +0530

    Improved flexibity

--- add_software.sh
+++ add_software.sh
@@ -1,29 +1,50 @@
+########################################################################
+# Program to allow admins to add more softwares to the main.yml file
+#
+# The program takes the packages as CLAs
+# e.g. ./add_software package1 package2
+#
+##########################################################################
 
+#check if atleast 1 package has been specified
 if [[ $# -lt 1 ]]
 	then
 	 	echo "Atleast one package has to specified as CLA"
 fi
 
+
+# go through each of the packages one by one
 for i in $@
 do
+	# check if such a package is available in the apt-get cache
 	apt-cache show $i > /dev/null 2>&1
+
+	# if such a package doesn't exists, notify the user about it
 	if [[ $? -ne 0 ]]
 	then
-		echo "#############################################"
+		echo "****************************************************"
 		echo " THE PACKAGE '$i' IS NOT PRESENT IN APT_GET REPOSITORY"
 		echo " PLEASE ENTER A VALID PACKAGE NAME"
+	
+	# if such a package exists, check if the package is already present 
+	# in the main.yml file
 	else
+		# to check if the package is already present in main.yml
 		cat roles/basic/tasks/main.yml | grep -B100 /etc/apt | grep $i > /dev/null
+		
+		# if not present, add it to the main.yml file
 		if [[ $? -ne 0 ]]
 			then
-				awk -v i="  - $i" '{print $0;if(NR==4){print i}}' roles/basic/tasks/main.yml > temp.txt
+				awk -v i="  - $i" '{print $0;if(NR==11){print i}}' roles/basic/tasks/main.yml > temp.txt
 				echo " Package $i has been added to main.yml"
+				cp temp.txt roles/basic/tasks/main.yml
+			# if already present, ignore
 			else
 				echo " Package $i already present in the main.yml"
 			fi
 		fi
 		echo
-		cp temp.txt roles/basic/tasks/main.yml
+		
 	done
 
 rm temp.txt
